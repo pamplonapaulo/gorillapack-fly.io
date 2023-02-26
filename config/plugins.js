d4c7e897b4eceebc65a54f11263d3ae5c801ec1a
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2023-02-26 16:42:05 -0300

    sendgrid email setup

--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,16 @@ module.exports = ({ env }) => ({
     enabled: true,
     resolve: "./src/plugins/melhorenvio",
   },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'adm.gorillapack@gmail.com',
+        defaultReplyTo: 'adm.gorillapack@gmail.com',
+      },
+    },
+  },
 });
